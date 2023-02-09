675f31ccef8f2df298c5d2f5809cc44636410aa7
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-02-09 14:46:18 -0300

    configuração do status bar

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { StatusBar } from "expo-status-bar";
+import { StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { Routes } from "./src/routes";
 export default function App() {
   return (
     <NavigationContainer>
+      <StatusBar backgroundColor="#008080" barStyle="light-content" />
       <Routes />
     </NavigationContainer>
   );
