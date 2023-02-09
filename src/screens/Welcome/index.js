0adec0148707c784a3555dcb0fcbc8ef9bcced56
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-02-09 14:47:35 -0300

    criação da página inicial

--- src/screens/Welcome/index.js
+++ src/screens/Welcome/index.js
@@ -1,9 +1,24 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 export function Welcome() {
   return (
-    <View>
-      <Text>Tela bem vindo</Text>
+    <View style={styles.container}>
+      <View style={styles.containerLogo}>
+        <Image
+          source={require("../../assets/png-clipart-logo-graphic-designer-business-gps-logo-logo-pharmaceutical-drug.png")}
+          style={{ width: "100%" }}
+          resizeMode="center"
+        />
+      </View>
+      <View style={styles.containerForm}>
+        <Text>Localize-se onde estiver!</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {},
+  containerLogo: {},
+  containerForm: {},
+});
