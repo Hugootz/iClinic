98f46a6fa27ccf820d9c3e36316ad05a0f6016bd
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-02-13 13:24:55 -0300

    importação de fontes personalizadas

--- App.js
+++ App.js
@@ -2,7 +2,24 @@ import React from "react";
 import { StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { Routes } from "./src/routes";
+import * as SplashScreen from "expo-splash-screen";
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_300Light,
+} from "@expo-google-fonts/poppins";
 export default function App() {
+  SplashScreen.preventAutoHideAsync();
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_300Light,
+  });
+  if (!fontsLoaded) {
+    return null;
+  }
+  SplashScreen.hideAsync();
   return (
     <NavigationContainer>
       <StatusBar backgroundColor="#014ba0" barStyle="light-content" />
