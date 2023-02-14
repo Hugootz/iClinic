921002482d88b7dcc956f1468f4666094886af66
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-02-14 13:02:48 -0300

    introdução da fonte e navegação na página

--- src/screens/Welcome/index.js
+++ src/screens/Welcome/index.js
@@ -1,9 +1,27 @@
 import React from "react";
+import * as SplashScreen from "expo-splash-screen";
 import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 import * as Animatable from "react-native-animatable";
-import {} from "@expo-google-fonts/poppins";
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_300Light,
+} from "@expo-google-fonts/poppins";
+import { useNavigation } from "@react-navigation/native";
 
 export function Welcome() {
+  const navigation = useNavigation();
+  SplashScreen.preventAutoHideAsync();
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_300Light,
+  });
+  if (!fontsLoaded) {
+    return null;
+  }
+  SplashScreen.hideAsync();
   return (
     <View style={styles.container}>
       <View style={styles.containerLogo}>
@@ -14,11 +32,18 @@ export function Welcome() {
           resizeMode="contain"
         />
       </View>
-      <Text style={styles.textImage}>iClinic</Text>
-      <Animatable.View animation="fadeInUp" style={styles.containerForm}>
+
+      <Animatable.View
+        delay={600}
+        animation="fadeInUp"
+        style={styles.containerForm}
+      >
         <Text style={styles.title}>Localize-se de onde estiver!</Text>
         <Text style={styles.text}>Faça o login para começar</Text>
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("SignIn")}
+          style={styles.button}
+        >
           <Text style={styles.buttonText}>Acessar</Text>
         </TouchableOpacity>
       </Animatable.View>
@@ -34,14 +59,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  textImage: {
-    color: "#fff",
-    alignSelf: "center",
-    bottom: 40,
-    fontSize: 30,
-    fontWeight: "bold",
-    fontFamily: "Poppins_500Medium",
-  },
+
   containerForm: {
     flex: 1,
     backgroundColor: "#fff",
@@ -52,24 +70,28 @@ const styles = StyleSheet.create({
   title: {
     color: "#014ba0",
     fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 12,
+
     textAlign: "center",
+    fontFamily: "Poppins_500Medium",
   },
   text: {
     color: "#014ba0",
     fontSize: 18,
     textAlign: "center",
-    padding: 45,
-    fontWeight: "bold",
+    padding: 15,
+    fontFamily: "Poppins_400Regular",
   },
   button: {
     position: "absolute",
     backgroundColor: "#014ba0",
     borderRadius: 50,
-    padding: 30,
+    padding: 25,
     alignSelf: "center",
-    bottom: "15%",
+    bottom: "10%",
   },
-  buttonText: { color: "#ffffff", fontSize: 20, fontWeight: "bold" },
+  buttonText: {
+    color: "#ffffff",
+    fontSize: 20,
+    fontFamily: "Poppins_500Medium",
+  },
 });
