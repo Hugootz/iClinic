533c1dfbccbc016105bff7ac4908b319ccb70266
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-03-07 18:19:14 -0300

    função de voltar página no componente

--- src/components/BackButton/index.js
+++ src/components/BackButton/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { TouchableOpacity } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 
 export function BackButton() {
+  const navigation = useNavigation();
   return (
-    <TouchableOpacity>
-      <AntDesign name="left" size={24} color={"#fff"} />
+    <TouchableOpacity onPress={() => navigation.goBack()}>
+      <AntDesign name="left" size={20} color={"#fff"} />
     </TouchableOpacity>
   );
 }
