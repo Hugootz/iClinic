aeacc09648e4529b540c326bf735340a1ba2bbb5
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-03-14 17:22:11 -0300

    criação de rotas para autenticação

---
+++ src/routes/AppStack.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import { Welcome } from "../screens/Welcome";
+const Stack = createNativeStackNavigator();
+export function AppStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Welcome" component={Welcome} />
+    </Stack.Navigator>
+  );
+}
