66589450b3bd0a5c22f1bba509ed6c1d97d6089b
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-03-15 10:19:28 -0300

    mudança na lógica de navegação

--- src/routes/AuthStack.js
+++ src/routes/AuthStack.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { SignIn } from "../screens/SignIn";
+import { Welcome } from "../screens/Welcome";
 const Stack = createNativeStackNavigator();
-export function AppStack() {
+export function AuthStack() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="SignIn" component={SignIn} />
