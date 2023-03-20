846be3ee7e3fec7533cc98eb43b2d7a6c76388d8
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-03-20 14:37:43 -0300

    introdução de rotas

--- src/routes/AppStack.js
+++ src/routes/AppStack.js
@@ -2,11 +2,13 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import { Welcome } from "../screens/Welcome";
+import { Register } from "../screens/Register";
 const Stack = createNativeStackNavigator();
 export function AppStack() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Welcome" component={Welcome} />
+      <Stack.Screen name="Register" component={Register} />
     </Stack.Navigator>
   );
 }
