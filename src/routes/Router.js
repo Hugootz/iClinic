38d4ea5c196dd95dd246470ff3987797a35d564a
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-03-20 14:36:24 -0300

    introdução as rotas e a lógica para login

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,7 +1,20 @@
-import React from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React, { useContext } from "react";
 import { NavigationContainer } from "@react-navigation/native";
+
 import { AppStack } from "./AppStack";
 import { AuthStack } from "./AuthStack";
-const Stack = createNativeStackNavigator();
-export function Router() {}
+import { AuthContext } from "../contexts/Auth";
+import { useAuth } from "../contexts/Auth";
+
+export function Routes() {
+  const { authData } = useAuth();
+
+  return (
+    <NavigationContainer
+      screenOptions={{ headerShown: false }}
+      independent={true}
+    >
+      {authData ? <AppStack /> : <AuthStack />}
+    </NavigationContainer>
+  );
+}
