4782a63e7756cca077bd2b863b5167756921836e
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-03-20 14:37:14 -0300

    retirada de importação

--- src/routes/AuthStack.js
+++ src/routes/AuthStack.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { SignIn } from "../screens/SignIn";
-import { Welcome } from "../screens/Welcome";
+
 const Stack = createNativeStackNavigator();
 export function AuthStack() {
   return (
