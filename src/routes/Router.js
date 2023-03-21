193900212bdafd8fd4fd7b34425d8f0cfe1ce9e3
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-03-21 15:46:47 -0300

    mudança nas configurações de rota

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -3,17 +3,14 @@ import { NavigationContainer } from "@react-navigation/native";
 
 import { AppStack } from "./AppStack";
 import { AuthStack } from "./AuthStack";
-import { AuthContext } from "../contexts/Auth";
+
 import { useAuth } from "../contexts/Auth";
 
 export function Routes() {
   const { authData } = useAuth();
 
   return (
-    <NavigationContainer
-      screenOptions={{ headerShown: false }}
-      independent={true}
-    >
+    <NavigationContainer independent={true}>
       {authData ? <AppStack /> : <AuthStack />}
     </NavigationContainer>
   );
