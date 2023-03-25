25e5b830259f474bb4d8ebb25fcefd448d3b16da
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-03-25 14:59:42 -0300

    mudança na navegação das telas não autenticadas

--- src/routes/AuthStack.js
+++ src/routes/AuthStack.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { SignIn } from "../screens/SignIn";
+import { Register } from "../screens/Register";
 
 const Stack = createNativeStackNavigator();
 export function AuthStack() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="SignIn" component={SignIn} />
+      <Stack.Screen name="Register" component={Register} />
     </Stack.Navigator>
   );
 }
