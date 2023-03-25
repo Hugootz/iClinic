06be65f1f4cf83fd0b62c279e117cfd943df41f4
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-03-25 15:00:11 -0300

    mudança de navegação das telas autenticadas

--- src/routes/AppStack.js
+++ src/routes/AppStack.js
@@ -2,13 +2,12 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import { Welcome } from "../screens/Welcome";
-import { Register } from "../screens/Register";
+
 const Stack = createNativeStackNavigator();
 export function AppStack() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Welcome" component={Welcome} />
-      <Stack.Screen name="Register" component={Register} />
     </Stack.Navigator>
   );
 }
